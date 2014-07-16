a8da331dfd8d6877c7f481ca1b1738ce0fc25a95
Andrew Or <andrewor14@gmail.com>
2014-07-15 17:30:01 -0700

    Update run-many-link-failures.sh to reflect new controller changes

--- ns-3-dev/scripts/run-many-link-failures.sh
+++ ns-3-dev/scripts/run-many-link-failures.sh
@@ -2,6 +2,6 @@
 
 for i in `seq 1 8`; do
   i=$i$i$i$i
-  screen -dmS grape-$i sh -c "DDC_SEED=$i scripts/run-link-failure.sh ft16.topo grape 8 25 60 3600 1640 60s 10ms 10us"
+  screen -dmS grape-$i sh -c "DDC_SEED=$i scripts/run-link-failure.sh ft16.topo grape 3 3600 1640 60s 10ms 10us"
   echo "Started running screen with seed $i"
 done
